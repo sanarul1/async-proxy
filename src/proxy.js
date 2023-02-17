a679cb6c9862ef47ca53672f4aef2598ca3cc563
sanarul1 <125356240+sanarul1@users.noreply.github.com>
2023-02-17 12:42:01 +0530

    mapper

--- src/proxy.js
+++ src/proxy.js
@@ -1,84 +1,17 @@
 const axios = require('axios');
+const merge = require("deepmerge");
 
-const dropQueryParams = ['url', 'expected'];
-
-const dropHeaders = ['host', "content-length"];
-
-const isValidUrl = (str) => {
-  try {
-    new URL(str);
-    return true;
-  } catch (err) {
-    return false;
-  }
-};
-
-const filterObj = (obj, keyArr) => {
-  let { ...clone } = obj;
-  keyArr.forEach(element => {
-    if (clone.hasOwnProperty(element)) {
-      delete clone[element];
-    }
-  });
-  return clone;
-};
-
-const cloneActual = (req) => {
-  const config = {};
-  config.headers = {};
-  let params = '';
-  let query = '';
-  if (req.query.url) {
-    let valid = isValidUrl(req.query.url);
-    if (!valid) {
-      throw new Error('[url] param value is incorrect');
-    }
-
-    if (req.params) {
-      params = '/' + Object.keys(req.params).map(function (key) {
-        return req.params[key];
-      }).join('/');
-    }
-
-    if (req.query) {
-      const queryClone = filterObj(req.query, dropQueryParams);
-      if (Object.keys(queryClone).length > 0) {
-        query = '?' + Object.keys(queryClone).map(function (key) {
-          return key + '=' + queryClone[key];
-        }).join('&');
-      }
-    }
-
-    config.url = req.query.url + params + query;
-  } else {
-    throw new Error('[url] param not specified');
-  }
-
-  config.method = req.method;
-
-  if (req.headers) {
-    const headersClone = filterObj(req.headers, dropHeaders);
-    config.headers = headersClone;
-  }
-
-  if (req.body) {
-    config.data = req.body;
-  }
-
-  console.log(config);
-  return config;
-};
-
-const proxy = async (req) => {
+const proxy = async (req, res) => {
   let data = {};
   try {
-    await axios(cloneActual(req))
+    const config = res.locals.config;
+    await axios(config)
       .then((response) => {
         //setTimeout(()=>{}, 20);
         console.log('Response from back-end');
         data.status = response.status;
         data.statusText = response.statusText;
-        data.body = response.data;
+        data.body = merge(res.locals.actual, response.data);
         console.log(data.body);
       })
       .catch(err => {
